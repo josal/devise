81c9a6521f4aa426ec71cad88c6cd84304294bbe
Jose <josese@gmail.com>
2011-01-09 08:28:20 +0100

    Deleting original create action

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -31,11 +31,11 @@ class Devise::SessionsController < ApplicationController
   # WITH JSON RESPONSE
 
   # POST /resource/sign_in
-  def create
-    resource = warden.authenticate!(:scope => resource_name, :recall => "new")
-    set_flash_message :notice, :signed_in
-    sign_in_and_redirect(resource_name, resource)
-  end
+  # def create
+  #   resource = warden.authenticate!(:scope => resource_name, :recall => "new")
+  #   set_flash_message :notice, :signed_in
+  #   sign_in_and_redirect(resource_name, resource)
+  # end
 
 
   def create
